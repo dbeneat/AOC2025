771ee43720567bf14b97647aec84f2f6a3cc031f
Donatien Beneat <donatien_beneat@yahoo.fr>
2025-12-05 16:05:25 +0100

    Change 2d array to array of structs

--- day05.c
+++ day05.c
@@ -2,52 +2,38 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int cmp(const void* a,const void* b){
-    const uint64_t (*row1)[2]=*(const uint64_t(**) [2]) a;
-    const uint64_t (*row2)[2]=*(const uint64_t(**) [2]) b;
-    return row1[0]<row2[0]?-1:1;
-}
-
+typedef struct {uint64_t a,b;} Range;
+int cmp(const Range* R,const Range* S){ return R->a<S->a?-1:1; }
 
 int main(void){
     uint64_t part1=0,part2=0;
-    uint64_t a,b,id;
-    int nint=0;
-    uint64_t interv[1500][2]={{0}};
-    while(scanf("%llu-%llu",&a,&b)==2){
-        interv[nint][0]=a; interv[nint][1]=b;
-        nint++;
-    }
-    while(scanf("%llu",&id)==1){
-        interv[nint][0]=id; interv[nint][1]=id-1;
-        nint++;
-    }
-    qsort((double**) interv,nint,2*sizeof(uint64_t),cmp);
-    uint64_t lenA=0,lenB=0,lenC=0;
+    int nr=0; 
+    Range T[1500]={{0}};
+    while(scanf("%llu-%llu",&T[nr].a,&T[nr].b)==2){ nr++; }
+    while(scanf("%llu",&T[nr].a)==1){ nr++; }
+    qsort(T,nr,sizeof(Range),(int(*)(const void*,const void*))cmp);
     // Sieve formula: len(A inter B) = len(A)+len(B)-len(A U B)
-    // lenA:total measure of intervals
-    // lenB: total measure of points
-    // lenC: total measure of union (A U B)
+    //    lenA: total measure of intervals
+    //    lenB: total measure of points
+    //    lenC: total measure of union (A U B)
+    uint64_t lenA=0,lenB=0,lenC=0;
     uint64_t oldb=0;
-    for(int i=0;i<nint;i++){
-        uint64_t a=interv[i][0],b=interv[i][1];
-        if(a>b){ lenB+=1; continue; }
+    for(int i=0;i<nr;i++){
+        uint64_t a=T[i].a,b=T[i].b;
+        if(b==0){ lenB+=1; continue; }
         if(b<=oldb){ continue; }
         if(a<=oldb){ a=oldb+1; }
-        lenA+=b-a+1;
-        oldb=b;
+        lenA+=b-a+1; oldb=b;
     }
     oldb=0;
-    for(int i=0;i<nint;i++){
-        uint64_t a=interv[i][0],b=interv[i][1];
-        if(a>b){ b=a; }
+    for(int i=0;i<nr;i++){
+        uint64_t a=T[i].a,b=T[i].b;
+        if(b==0){ b=a; }
         if(b<=oldb){ continue; }
         if(a<=oldb){ a=oldb+1; }
-        lenC+=b-a+1;
-        oldb=b;
+        lenC+=b-a+1; oldb=b;
     }
-    part1=lenA+lenB-lenC;
-    part2=lenA;   
+    part1=lenA+lenB-lenC; part2=lenA;
     printf("%llu %llu\n",part1,part2);
     return 0;
 }
