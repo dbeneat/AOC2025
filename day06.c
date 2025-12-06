a87e3a02b474e63262a40bb68c379d4714fe5e7b
Donatien Beneat <donatien_beneat@yahoo.fr>
2025-12-06 12:25:19 +0100

    Both parts, using a stack machine

--- day06.c
+++ day06.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct {uint64_t sum,mul;} Res;
+int sp=0;
+uint64_t stack[5]={0};
+uint64_t res=0;
+void calc(char c){
+    if(c>='0'){ stack[sp]=10*stack[sp]+c-'0'; }
+    else if(c=='+'){
+            res=0;
+            for(int i=0;i<=sp;i++){ res=res+stack[i]; }
+            sp=0; stack[sp]=0;
+    }
+    else if(c=='*'){
+            res=1;
+            for(int i=0;i<=sp;i++){ res=res*stack[i]; }
+            sp=0; stack[0]=0;
+    }
+    else if(c=='#' && stack[sp]>0){ sp++;stack[sp]=0; }
+}
 
 int main(void){
-    uint64_t part1=0;
-    Res T[2000]; for(int i=0;i<2000;i++){ T[i].sum=0;T[i].mul=1; }
-    char buff[5000];
-    char sep[3]=" \n";
+    uint64_t part1=0,part2=0;
+    char buff[5][4000]={{0}};
+    int nr=0;
+    while(fgets(buff[nr],4000,stdin)){ nr++; }
     int col=0;
-    while(fgets(buff,5000,stdin)){
-        //printf("\n\n");
-        char* tok=strtok(buff,sep);
-        while(tok){
-            
-            //printf("%d:%s ",col,tok);
-            //printf("[%u]",tok[0]);
-            switch(tok[0]){
-                case '+': T[col].mul=0; col++; break;
-                case '*': T[col].sum=0; col++; break;
-                default: {
-                    uint64_t n=_strtoui64(tok,NULL,10);
-                    T[col].sum+=n;T[col].mul*=n;
-                    col++;
-                }
-                break;
-            }
-            tok=strtok(NULL,sep);
+    while(buff[0][col]!=0 && buff[0][col]!='\n'){
+        int newcol=col+1;
+        while(buff[nr-1][newcol]==' '){ newcol++; }
+        for(int i=0;i<nr-1;i++){
+            calc('#');
+            for(int j=col;j<newcol;j++){ calc(buff[i][j]); } 
         }
-        col=0;
+        calc(buff[nr-1][col]);
+        part1+=res;
+        col=newcol;
     }
-    //for(int i=0;T[i].sum+T[i].mul>1 && i<2000;i++){printf("(%llu %llu)",T[i].sum,T[i].mul);}
-    for(int i=0;T[i].sum+T[i].mul>1 && i<2000;i++){ part1+=T[i].sum+T[i].mul; }
-    printf("%llu\n",part1);
+    int nc=col;
+    for(int j=nc-1;j>=0;j--){
+        calc('#');
+        for(int i=0;i<nr-1;i++){ calc(buff[i][j]); }
+        char op=buff[nr-1][j];
+        if(op=='+' || op=='*'){ calc(op); part2+=res; }    
+    }
     
+    printf("%llu %llu\n",part1,part2);
     return 0;
 }
